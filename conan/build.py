114dc338d444752f9430fed3c712e5a908450f46
Uilian Ries <uilianries@gmail.com>
2018-01-14 13:19:06 -0200

    Build on Appveyor (#4590)
    - Build Conan on success of master branch
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conan/build.py
+++ conan/build.py
@@ -20,6 +20,10 @@ def get_version_from_recipe():
     return get_value_from_recipe(r'''version\s*=\s*["'](\S*)["']''').groups()[0]
 
 
+def running_on_appveyor():
+    return os.getenv("APPVEYOR", False)
+
+
 def get_env_vars():
     username = os.getenv("CONAN_USERNAME", "flatbuffers")
     channel = os.getenv("CONAN_CHANNEL", "testing")
@@ -28,20 +32,51 @@ def get_env_vars():
     return name, version, username, channel
 
 
+def get_stable_branch_pattern():
+    return "master" if running_on_appveyor() else r"v\d+\.\d+\.\d+"
+
+
 if __name__ == "__main__":
     name, version, username, channel = get_env_vars()
     reference = "{0}/{1}".format(name, version)
     upload = "https://api.bintray.com/conan/{0}/conan".format(username)
+    stable_branch_pattern = get_stable_branch_pattern()
+    test_folder = "-tf %s" % os.path.join("conan", "test_package")
 
     builder = ConanMultiPackager(
-        args="-tf %s" % os.path.join("conan", "test_package"),
+        args=test_folder,
         username=username,
         channel=channel,
         reference=reference,
         upload=upload,
         remotes=upload,
         upload_only_when_stable=True,
-        stable_branch_pattern=r"v\d+\.\d+\.\d+")
+        stable_branch_pattern=stable_branch_pattern)
 
     builder.add_common_builds(shared_option_name="%s:shared" % name, pure_c=False)
+
+    if running_on_appveyor():
+        filtered_builds = []
+        ci_platform = os.getenv("Platform")
+        ci_platform = "x86_64" if ci_platform == "x64" else "x86"
+        ci_configuration = os.getenv("Configuration")
+        ci_worker = os.getenv("APPVEYOR_BUILD_WORKER_IMAGE")
+        if ci_worker == "Visual Studio 2015":
+            ci_worker = ["12", "14"]
+        elif ci_worker == "Visual Studio 2017":
+            ci_worker = ["15"]
+
+        for settings, options, env_vars, build_requires, reference in builder.items:
+            if settings.build_type != ci_configuration:
+                continue
+            if settings.arch != ci_platform:
+                continue
+            if settings.compiler.version not in ci_worker:
+                continue
+
+            filtered_builds.append([settings, options, env_vars, build_requires])
+
+        builder.builds = filtered_builds
+
+
     builder.run()
