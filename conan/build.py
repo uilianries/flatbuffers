07d04f6f0763a8ab1f17bc8db4668d36235f9310
Uilian Ries <uilianries@gmail.com>
2018-01-14 13:19:06 -0200

    Build on Appveyor (#4590)
    - Build Conan on success of master branch
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conan/build.py
+++ conan/build.py
@@ -20,6 +20,10 @@ def get_version_from_recipe():
     return get_value_from_recipe(r'''version\s*=\s*["'](\S*)["']''').groups()[0]
 
 
+def running_on_appveyor():
+    return os.getenv("APPVEYOR", False)
+
+
 def get_env_vars():
     username = os.getenv("CONAN_USERNAME", "flatbuffers")
     channel = os.getenv("CONAN_CHANNEL", "testing")
@@ -28,20 +32,40 @@ def get_env_vars():
     return name, version, username, channel
 
 
+def get_stable_branch_pattern():
+    return "master" if running_on_appveyor() else r"v\d+\.\d+\.\d+"
+
+
+def set_appveyor_environment():
+    compiler_version = os.getenv("CMAKE_VS_VERSION").split(" ")[0].replace('"', '')
+    os.environ["CONAN_VISUAL_VERSIONS"] = compiler_version
+
+    ci_platform = os.getenv("Platform").replace('"', '')
+    ci_platform = "x86" if ci_platform == "x86" else "x86_64"
+    os.environ["CONAN_ARCHS"] = ci_platform
+
+    os.environ["CONAN_BUILD_TYPES"] = os.getenv("Configuration").replace('"', '')
+
+
 if __name__ == "__main__":
     name, version, username, channel = get_env_vars()
     reference = "{0}/{1}".format(name, version)
     upload = "https://api.bintray.com/conan/{0}/conan".format(username)
+    stable_branch_pattern = get_stable_branch_pattern()
+    test_folder = "-tf %s" % os.path.join("conan", "test_package")
+
+    if running_on_appveyor():
+        set_appveyor_environment()
 
     builder = ConanMultiPackager(
-        args="-tf %s" % os.path.join("conan", "test_package"),
+        args=test_folder,
         username=username,
         channel=channel,
         reference=reference,
         upload=upload,
         remotes=upload,
         upload_only_when_stable=True,
-        stable_branch_pattern=r"v\d+\.\d+\.\d+")
+        stable_branch_pattern=stable_branch_pattern)
 
     builder.add_common_builds(shared_option_name="%s:shared" % name, pure_c=False)
     builder.run()
