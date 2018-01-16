7908e19babbb39794a18c726f908eb20dd434cdc
Uilian Ries <uilianries@gmail.com>
2018-01-16 21:56:16 -0200

    Reduce Conan CI support to simple scripts (#4590)
    - Removed msvc 10 x86_64 workaround
    - Updated conan remote address
    - Added Bincrafters' package tools
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conan/travis/install.sh
+++ conan/travis/install.sh
@@ -19,5 +19,5 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate conan
 fi
 
-pip install conan_package_tools # It install conan too
+pip install conan_package_tools bincrafters-package-tools conan
 conan user
